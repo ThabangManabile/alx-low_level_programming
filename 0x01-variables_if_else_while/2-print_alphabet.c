30fa1f2775540b5eefa32bd24162a28ddfb9007a
ThabangM00 <manabiletm@gmail.com>
2023-02-20 11:17:13 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * main -Entry
+ * main - Entry
  * Return: 0
+ *
  */
 
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz",
-	     int i;
+	char i;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
