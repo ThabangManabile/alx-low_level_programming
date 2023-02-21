fabc3c48e3eac32d8866d3c167318dccc4c8b74d
ThabangM00 <manabiletm@gmail.com>
2023-02-21 01:25:23 -0800

    0-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o\n");
+
+	return (0);
+}
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - testing positive or negative if function
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i= 98;
+	positive_or_negative(i);
+
+	return (0);
+}
